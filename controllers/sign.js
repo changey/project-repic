b6bb7a82dcef843dcdd7013fd70a4a98953f29d8
Eric Chang <changey@mit.edu>
2015-04-19 15:23:35 -0700

    added proxy

--- controllers/sign.js
+++ controllers/sign.js
@@ -1,12 +1,3 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var utility = require('utility');
-require('../config.js');
-
-var userSchema = new Schema({
-  name: { type: String},
-  loginname: { type: String},
-  pass: { type: String },
-  email: { type: String},
-  profile_image_url: {type: String}
-});
+exports.signup = function(req, res) {
+    
+}
---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var utility = require('utility');
+require('../config.js');
+
+var UserSchema = new Schema({
+  name: { type: String},
+  loginname: { type: String},
+  pass: { type: String },
+  email: { type: String},
+  profile_image_url: {type: String}
+});
+
+mongoose.model('User', UserSchema);
