66570987b5829b78ea8457225bdbf0a9dc459584
Eric Chang <changey@mit.edu>
2015-04-19 20:33:28 -0700

    added bcrypt

--- controllers/sign.js
+++ controllers/sign.js
@@ -1,3 +1,39 @@
+var User = require('../proxy').User;
+
 exports.signup = function(req, res) {
-    
+  var loginname = req.body.loginname;
+  var email = req.body.email;
+  var pass = req.body.pass;
+  var rePass = req.body.re_pass;
+
+  var ep = new eventproxy();
+  User.getUsersByQuery({'$or': [
+    {'loginname': loginname},
+    {'email': email}
+  ]}, {}, function (err, users) {
+    if (err) {
+      return next(err);
+    }
+    if (users.length > 0) {
+      ep.emit('prop_err', '帳戶名或電子信箱已被使用。');
+      return;
+    }
+
+    tools.bhash(pass, ep.done(function (passhash) {
+
+      User.newAndSave(loginname, loginname, passhash, email, avatarUrl, false, function (err) {
+        if (err) {
+          console.log(err)
+          //return next(err);
+        } else {
+          console.log("success")
+        }
+        // 发送激活邮件
+        // mail.sendActiveMail(email, utility.md5(email + passhash + config.session_secret), loginname);
+        // res.render('sign/signup', {
+        //   success: '歡迎加入 ' + config.name + ' 華航哩程數、長榮哩程數‧拍賣交換！'
+        // });
+      });
+
+    }));
 }
--- proxy/user.js
+++ proxy/user.js
@@ -1,3 +1,14 @@
 var models = require('../models');
 var User = models.User;
 var utility = require('utility');
+
+exports.newAndSave = function(name, loginname, pass, email, avatar_url, active, callback) {
+  var user = new User();
+  user.name = loginname;
+  user.loginname = loginname;
+  user.pass = pass;
+  user.email = email;
+  user.avatar = avatar_url;
+  user.active = active || false;
+  user.save(callback);
+};
